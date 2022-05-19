42c1f721badc5352e6f7f8923c3a3c7819f07173
Alex Hoskins <alexander.hoskins@yahoo.com>
2022-05-19 14:10:37 -0700

    Updated comments

--- Replace_With_Alphabet_Position.js
+++ Replace_With_Alphabet_Position.js
@@ -1,32 +1,33 @@
-// Welcome.
+/*Welcome.
 
-// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
-// If anything in the text isn't a letter, ignore it and don't return it.
+If anything in the text isn't a letter, ignore it and don't return it.
 
-// "a" = 1, "b" = 2, etc.
+"a" = 1, "b" = 2, etc.
 
-// Example
-// alphabetPosition("The sunset sets at twelve o' clock.")
-// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
-//PREP
-// PARAMETERS
-// Will this always be a string? 
-// Will it ever be a number, array, etc...
-// Will it ever be an empty string?
-// RETURNS
-// If it is a number, space, anything that's not a letter, we should ignore it in the return?
-// So a correct output will have spaces in between the numbers?
-// EXAMPLES
-// alphabetPosition('aaa') should return ('1 1 1')
-// alphabetPosition('a 7 b c 8 9 7') should return ('1 2 3')
-// PSEUDOCODE
-// For loop over the string
-// Create match regex
-// If character is a letter, then I will find the value of that character
-// add character value to a string
+PARAMETERS
+Will this always be a string? 
+Will it ever be a number, array, etc...
+Will it ever be an empty string?
+RETURNS
+If it is a number, space, anything that's not a letter, we should ignore it in the return?
+So a correct output will have spaces in between the numbers?
+EXAMPLES
+alphabetPosition('aaa') should return ('1 1 1')
+alphabetPosition('a 7 b c 8 9 7') should return ('1 2 3')
+PSEUDOCODE
+For loop over the string
+Create match regex
+If character is a letter, then I will find the value of that character
+add character value to a string
+*/
 
+//Answer
 
 function alphabetPosition(text) {
     let characterValueText=''
