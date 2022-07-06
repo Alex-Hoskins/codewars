d3bfecb92334b75f9f2de02ef872ff48e8172007
Alex Hoskins <alexander.hoskins@yahoo.com>
2022-07-06 00:23:44 -0700

    mergeSort

--- Recursion.js
+++ Recursion.js
@@ -28,3 +28,41 @@ console.log(recFact(5))
 
 //pitfalls - no base or incorrect return or stackoverflow
 // helper function recursion - outer function is not recursive with an inner helper function that is recursive
+
+
+function merge(array1, array2){
+    let i = 0
+    let j = 0
+    let results = []
+    while(i < array1.length && j < array2.length){
+        if(array1[i]<=array2[j]){
+            results.push(array1[i])
+            i++
+        }else{
+            results.push(array2[j])
+            j++
+        }
+    }
+    while(i < array1.length){
+        results.push(array1[i])
+        i++
+    }
+     while(j < array2.length){
+        results.push(array2[j])
+        j++
+    }
+    
+    return results
+}
+
+console.log(merge([1,3,4,5,6,7,10],[1,2,7,8,9,11,12,13]))
+
+function mergeSort(arr){
+    if(arr.length <= 1){ return arr }
+    let mid = Math.floor(arr.length/2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    return merge(left, right)
+}
+
+console.log(mergeSort([1,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77,33,55,4,77,43,3453,44,66,7,77]))
